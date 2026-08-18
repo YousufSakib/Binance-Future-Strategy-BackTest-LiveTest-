a02189e3f9d8efee87d76db84afd2cad2d6b1662
Shakib <yousufsakib29603@gmail.com>
2026-08-18 23:51:32 +0600

    params combination updated

--- src/test/simple-param-combination.js
+++ src/test/simple-param-combination.js
@@ -1,12 +1,6 @@
 export function create_param_combinations({ slRate, tpRate, windowSize }) {
     const arr = [
         { slRate: 0.02, tpRate: 0.02, windowSize: 120 },
-        { slRate: 0.02, tpRate: 0.001, windowSize: 30 },
-        { slRate: 0.02, tpRate: 0.01, windowSize: 10 },
-        { slRate: 0.02, tpRate: 1, windowSize: 500 },
-        { slRate: 0.02, tpRate: 0.01, windowSize: 15 },
-        { slRate: 0.02, tpRate: 0.00001, windowSize: 5 },
-
     ]
 
     return arr.map(e => ({ ...e, key: JSON.stringify(e) }))
